48800a7e570e1f81d832a68d7fb50fe671add355
akshatgyl <akshatgyl@gmail.com>
2016-07-29 21:16:17 -0700

    Initial Commit

---
+++ Tutorios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tutorios
+//
+//  Created by Akshat Goyal on 7/29/16.
+//  Copyright © 2016 Tutorios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
